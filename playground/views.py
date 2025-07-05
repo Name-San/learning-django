deb8a88e090d454ed6c538753ea3a331d135fab4
Name-san <eman.jandog@gmai.com>
2025-07-06 01:12:38 +0800

    sorting and filtering query done

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q, F
+from store.models import Product, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'hello.html')
+    products = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    return render(request, 'hello.html', {'name': 'Eman', 'products': products})
