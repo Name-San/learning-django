83dfaf6ee5524286b41a74c31b2c461fea9d9d41
Name-san <eman.jandog@gmai.com>
2025-07-06 21:40:05 +0800

    select_related and prefetch related part.

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 # Create your views here.
 def say_hello(request):
-    products = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    # products = Product.objects.values('id', 'title', 'collection__title')
+    # products = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    products = OrderItem.objects.select_related('order__customer', 'product').order_by('-order__id')
+        
     return render(request, 'hello.html', {'name': 'Eman', 'products': products})
