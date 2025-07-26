a24ad66f41554de68a489289eff3f91ea874b0c9
Name-San <eman.jandog@gmail.com>
2025-07-26 14:23:54 +0800

    done with pagination

--- store/views.py
+++ store/views.py
@@ -12,6 +12,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .filters import ProductFilterSet
+from .pagination import DefaultPagination
 
 
 class ProductViewSet(ModelViewSet):
@@ -19,6 +20,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilterSet
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
