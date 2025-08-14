23720834b77312d03a4622446573d37cd463d491
eman-jandog <eman.jandog@gmail.com>
2025-08-14 20:43:18 +0800

    add: create test functions

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -15,3 +15,21 @@ class TestCollection:
     def test_if_user_is_anonymous_returns_401(self, create_collection):
         response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, create_collection, authenticate):
+        authenticate()        
+        response = create_collection({'title': 'a'})
+        assert response.status_code != status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_valid_returns_400(self, create_collection, authenticate):
+        authenticate(True)
+        response = create_collection({'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_if_collection_is_created_returns_200(self, authenticate, create_collection):
+        authenticate(True)
+
+        response = create_collection({'title': 'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
