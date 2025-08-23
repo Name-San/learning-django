4263717e0475ca60c8edf1cf2ce23bcf83ea823f
eman-jandog <eman.jandog@gmail.com>
2025-08-23 23:01:36 +0800

    teat: add test in retrieving product

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -81,12 +81,44 @@ class TestRetrieveCollection:
 class TestProducts:
     def test_if_anonymous_returns_401(self, authenticate, create_product):
         response = create_product({'title': "Sample Product", "unit_price": 3, "inventory": 11, 'collection': 2})
-
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_unauthenticated_user_returns_403(self, create_collection, authenticate):
+        authenticate()
+        response = create_collection({'title': 'Bumble Bee'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_if_authenticated_user_returns_200(self, authenticate, api_client):
         authenticate(True)
-
         response = api_client.get('/store/products/', {'title':'Transformer', 'unit_price': 1})
-
         assert response.status_code == status.HTTP_200_OK
+
+    def test_if_invalid_product_returns_400(self, authenticate, create_product):
+        authenticate(True)
+        response = create_product({'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+@pytest.mark.django_db
+class TestRetrieveProduct:
+    def test_if_product_exist_returns_200(self, api_client):
+        product = baker.make(Product)
+        response = api_client.get(f'/store/products/{product.id}/')
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_product_updated_returns_200(self, api_client, authenticate):
+        product = baker.make(Product)
+        authenticate(True)
+        response = api_client.put(f'/store/products/{product.id}/', 
+                                  {
+                                    "title": "Gundam",
+                                    "slug": "gundam",
+                                    "description": "Gundam figurine",
+                                    "unit_price": 4.0,
+                                    "inventory": 1,
+                                    "collection": 1,
+                                })
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['title'] == 'Gundam'
+        
+
+
